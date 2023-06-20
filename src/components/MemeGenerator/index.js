5b8ae4b619717abc3b749329c54d63e501b8cc3c
jayakrishnavamsi24 <jayakrishnavamsi24@gmail.com>
2023-06-20 19:09:32 +0000

    pushed code

---
+++ src/components/MemeGenerator/index.js
@@ -0,0 +1,163 @@
+import {Component} from 'react'
+import {
+  MemeImgContainer,
+  BgContainer,
+  MainTitle,
+  InputContainer,
+  LabelText,
+  InputText,
+  SelectText,
+  GenerateButton,
+  MemeText,
+  FormMemeLayout,
+  FormContainer,
+  OptionText,
+} from './styledComponents'
+
+const fontSizesOptionsList = [
+  {
+    optionId: '8',
+    displayText: '8',
+  },
+  {
+    optionId: '12',
+    displayText: '12',
+  },
+  {
+    optionId: '16',
+    displayText: '16',
+  },
+  {
+    optionId: '20',
+    displayText: '20',
+  },
+  {
+    optionId: '24',
+    displayText: '24',
+  },
+  {
+    optionId: '28',
+    displayText: '28',
+  },
+  {
+    optionId: '32',
+    displayText: '32',
+  },
+]
+
+class MemeGenerator extends Component {
+  state = {
+    imgUrl: '',
+    topText: '',
+    bottomText: '',
+    fontSize: fontSizesOptionsList[0].optionId,
+    isFormSubmitted: false,
+  }
+
+  onsubmitSuccess = () => {
+    this.setState({isFormSubmitted: true})
+  }
+
+  onSubmitForm = event => {
+    event.preventDefault()
+    const {imgUrl, topText, bottomText} = this.state
+    if (imgUrl !== '' && topText !== '' && bottomText !== '') {
+      this.onsubmitSuccess()
+    }
+  }
+
+  onChangeUrl = event => {
+    if (event.target.value === '') {
+      this.setState({imgUrl: event.target.value, isFormSubmitted: false})
+    } else {
+      this.setState({imgUrl: event.target.value})
+    }
+  }
+
+  onChangeTopText = event => {
+    if (event.target.value === '') {
+      this.setState({topText: event.target.value, isFormSubmitted: false})
+    } else {
+      this.setState({topText: event.target.value})
+    }
+  }
+
+  onChangeBottomText = event => {
+    if (event.target.value === '') {
+      this.setState({bottomText: event.target.value, isFormSubmitted: false})
+    } else {
+      this.setState({bottomText: event.target.value})
+    }
+  }
+
+  onChangeFontSize = event => {
+    this.setState({fontSize: event.target.value})
+  }
+
+  render() {
+    const {imgUrl, topText, bottomText, fontSize, isFormSubmitted} = this.state
+    return (
+      <BgContainer>
+        <MainTitle>Meme Generator</MainTitle>
+        <FormMemeLayout>
+          {isFormSubmitted && (
+            <MemeImgContainer bgImage={imgUrl} data-testid="meme">
+              <MemeText fontSize={fontSize}>{topText}</MemeText>
+              <MemeText fontSize={fontSize}>{bottomText}</MemeText>
+            </MemeImgContainer>
+          )}
+          <FormContainer onSubmit={this.onSubmitForm}>
+            <InputContainer>
+              <LabelText htmlFor="img-url">Image URL</LabelText>
+              <InputText
+                type="text"
+                id="img-url"
+                onChange={this.onChangeUrl}
+                value={imgUrl}
+              />
+            </InputContainer>
+            <InputContainer>
+              <LabelText htmlFor="top-text">Top Text</LabelText>
+              <InputText
+                type="text"
+                id="top-text"
+                onChange={this.onChangeTopText}
+                value={topText}
+              />
+            </InputContainer>
+            <InputContainer>
+              <LabelText htmlFor="bottom-text">Bottom Text</LabelText>
+              <InputText
+                type="text"
+                id="bottom-text"
+                onChange={this.onChangeBottomText}
+                value={bottomText}
+              />
+            </InputContainer>
+            <InputContainer>
+              <LabelText htmlFor="font-size">Bottom Text</LabelText>
+              <SelectText
+                id="font-size"
+                onChange={this.onChangeFontSize}
+                value={fontSize}
+              >
+                {fontSizesOptionsList.map(eachFontData => (
+                  <OptionText
+                    value={eachFontData.optionId}
+                    selected={eachFontData.optionId === '8'}
+                    key={eachFontData.optionId}
+                  >
+                    {eachFontData.displayText}
+                  </OptionText>
+                ))}
+              </SelectText>
+            </InputContainer>
+            <GenerateButton type="submit">Generate</GenerateButton>
+          </FormContainer>
+        </FormMemeLayout>
+      </BgContainer>
+    )
+  }
+}
+
+export default MemeGenerator
---
+++ src/components/MemeGenerator/styledComponents.js
@@ -0,0 +1,104 @@
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+  min-height: 100vh;
+  padding: 20px;
+  padding-top: 40px;
+  padding-bottom: 80px;
+  display: flex;
+  flex-direction: column;
+  font-family: 'Open Sans';
+  @media screen and (min-width: 768px) {
+    padding: 50px;
+    padding-top: 60px;
+    padding-bottom: 100px;
+  }
+`
+
+export const MainTitle = styled.h1`
+  text-align: center;
+  color: #35469c;
+  font-size: 25px;
+  @media screen and (min-width: 768px) {
+    text-align: left;
+  }
+`
+
+export const FormMemeLayout = styled.div`
+  @media screen and (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
+
+export const MemeImgContainer = styled.div`
+  background-image: url(${props => props.bgImage});
+  background-size: cover;
+  background-position: center;
+  height: 330px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  text-align: center;
+  @media screen and (min-width: 768px) {
+    width: 46%;
+    order: 2;
+  }
+`
+
+export const MemeText = styled.h1`
+  color: #ffffff;
+  font-size: ${props => props.fontSize}px;
+  font-weight: 500;
+`
+
+export const FormContainer = styled.form`
+  @media screen and (min-width: 768px) {
+    width: 46%;
+  }
+`
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+export const LabelText = styled.label`
+  color: #7e858e;
+  font-size: 14px;
+  margin-bottom: 5px;
+  margin-top: 20px;
+`
+export const InputText = styled.input`
+  height: 35px;
+  border: 1px solid #d7dfe9;
+  font-size: 16px;
+  padding: 10px;
+  color: #5a7184;
+`
+
+export const SelectText = styled.select`
+  height: 35px;
+  border: 1px solid #d7dfe9;
+  font-size: 16px;
+  padding-left: 10px;
+`
+
+export const OptionText = styled.option`
+  color: #1e293b;
+`
+
+export const GenerateButton = styled.button`
+  color: #ffffff;
+  background-color: #0b69ff;
+  width: 100%;
+  height: 40px;
+  border: none;
+  border-radius: 5px;
+  margin-top: 30px;
+  cursor: pointer;
+  outline: none;
+`
